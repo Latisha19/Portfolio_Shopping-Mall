ef999ce107209b7e0941d0a0a497b769dcef4514
linshan <linshan121419@gmail.com>
2024-08-19 00:03:05 +0800

    feat 加入 Categories 表對應

---
+++ Models/Categories.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCC.Models
+{
+    public class Categories
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public Categories()
+        {
+            CategoryName = string.Empty;
+            Description = string.Empty;
+        }
+        public Categories(int _categoryId, string _categoryName, string _description)
+        {
+            CategoryId = _categoryId;
+            CategoryName = _categoryName;
+            Description = _description;
+        }
+    }
+}
--- 建立&插入資料.sql
+++ 建立&插入資料.sql
@@ -2,17 +2,40 @@
 
 --CREATE TABLE "Orders" (
 --	orderId INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
---	category varchar(50),
+--	categoryId varchar(50),
 --	"name" varchar(50),
 --	price INT,
---	customer varchar(50)
+--	customer varchar(50),
+--	quantity INT NOT NULL
 --);
 
---INSERT INTO dbo.Orders (category, "name", price, customer)
---VALUES ('Fruit', 'Apple', 25, '小名'),
---		('Fruit', 'Banana', 50, '小華'),
---		('Fruit', 'Pineapple', 30, '小英'),
---		('Fruit', 'Mango', 60, '小李'),
---		('Fruit', 'Guava', 30, '小張');
+--INSERT INTO dbo.Orders (categoryId, "name", price, customer, quantity)
+--VALUES ('Fruit', 'Apple', 25, '小名', 3),
+--		('Fruit', 'Banana', 50, '小華', 5),
+--		('Fruit', 'Pineapple', 30, '小英', 4),
+--		('Fruit', 'Mango', 60, '小李', 6),
+--		('Fruit', 'Guava', 30, '小張', 2);
 
 
+--CREATE TABLE "Categories" (
+--	"categoryId" "int" IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+--	"categoryName" nvarchar (15) NOT NULL ,
+--	"description" "ntext" NULL
+--);
+
+--INSERT "Categories"("categoryName","description") 
+--VALUES('Beverages','Soft drinks, coffees, teas, beers, and ales')
+--INSERT "Categories"("categoryName","description") 
+--VALUES('Condiments','Sweet and savory sauces, relishes, spreads, and seasonings')
+--INSERT "Categories"("categoryName","description") 
+--VALUES('Confections','Desserts, candies, and sweet breads')
+--INSERT "Categories"("categoryName","description") 
+--VALUES('Dairy Products','Cheeses')
+--INSERT "Categories"("categoryName","description") 
+--VALUES('Grains/Cereals','Breads, crackers, pasta, and cereal')
+--INSERT "Categories"("categoryName","description") 
+--VALUES('Meat/Poultry','Prepared meats')
+--INSERT "Categories"("categoryName","description") 
+--VALUES('Produce','Dried fruit and bean curd')
+--INSERT "Categories"("categoryName","description") 
+--VALUES('Seafood','Seaweed and fish');
