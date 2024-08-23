f60f76dfad8e590a882b015e41b66183c59f8ff0
linshan <linshan121419@gmail.com>
2024-08-24 00:12:29 +0800

    feat 調整使用者權限：未登入與登入可觀看的頁面不同；調整原本 orders list 在首頁，改為導覽列區點按才能看到

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,35 +12,15 @@ namespace MVCCC.Controllers
 {
     public class HomeController : Controller
     {
+        //[Authorize]
         public ActionResult Index()
         {
-            DateTime date = DateTime.Now;
+            //TestMVCCC_Context db = new TestMVCCC_Context();
+            //DBmanager dbmanager = new DBmanager(db);
 
-            TestMVCCC_Context db = new TestMVCCC_Context();
-            DBmanager dbmanager = new DBmanager(db);
-            List<OrderDTO> orders = dbmanager.GetOrders();
-            var categories = db.Categories
-                                .Select(c => new { c.CategoryId, c.CategoryName })
-                                .ToList();
+            ViewBag.Name = User.Identity.Name;
 
-            // 如果使用匿名類型，請定義相應的類型
-            var categoryList = categories.Select(c => new Category
-            {
-                CategoryId = c.CategoryId,
-                CategoryName = c.CategoryName
-            }).ToList();
-
-            // 創建 ViewModel
-            var viewModel = new TestMVCCCListViewModel
-            {
-                Orders = orders,
-                Categories = categoryList
-            };
-
-            ViewBag.Date = date;
-            ViewBag.Orders = orders;
-
-            return View(viewModel);
+            return View();
         }
 
         public ActionResult About()
--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -11,6 +11,44 @@ namespace MVCCC.Controllers
 {
     public class OrdersController : Controller
     {
+        #region Orders List Home
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult OrdersList()
+        {
+            TestMVCCC_Context db = new TestMVCCC_Context();
+            DBmanager dbmanager = new DBmanager(db);
+
+            DateTime date = DateTime.Now;
+
+            List<OrderDTO> orders = dbmanager.GetOrders();
+            var categories = db.Categories
+                                .Select(c => new { c.CategoryId, c.CategoryName })
+                                .ToList();
+
+            // 如果使用匿名類型，請定義相應的類型
+            var categoryList = categories.Select(c => new Category
+            {
+                CategoryId = c.CategoryId,
+                CategoryName = c.CategoryName
+            }).ToList();
+
+            // 創建 ViewModel
+            var viewModel = new TestMVCCCListViewModel
+            {
+                Orders = orders,
+                Categories = categoryList
+            };
+
+            ViewBag.Date = date;
+            ViewBag.Orders = orders;
+
+            return View(viewModel);
+        }
+
+        #endregion
+
         #region CREATE
 
         // GET: Orders
